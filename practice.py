d5aa1d490ff646d293218f7d8b0741e8257eea7d
Joseph Mena Lopez <jmena@macalester.edu>
2022-10-25 15:19:08 -0500

    added in basic starting functions to run the game and allow python scripts to control the game. not fully implemented yet

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
@@ -14,3 +15,69 @@ pyboy = PyBoy('ROMs/Super Mario Land.gb')
 while not pyboy.tick():
     pass
 pyboy.stop()
+
+class environment:
+    def __init__(self):
+        #setup to run the game
+        fileName = 'ROMs/Super Mario Land.gb'
+        quiet = "--quiet" in sys.argv
+        self.pyboy = PyBoy(fileName, window_type="headless" if quiet else "SDL2", window_scale=5, debug=quiet, game_wrapper=True)
+        self.pyboy.set_emulation_speed(1)
+        assert self.pyboy.cartridge_title() == "SUPER MARIOLAN"
+        
+        #game wrapper to help with hooking into the game
+        self.mario = self.pyboy.game_wrapper()
+        self.mario.start_game()
+        self.done = False
+        self.time = 10
+        
+        #makes sure game variables are probably set
+        assert self.mario.score == 0
+        assert self.mario.lives_left == 2
+        assert self.mario.world == (1, 1)
+        assert self.mario.fitness == 0
+        
+        #statespace
+        self.action_size = 5
+        state_full = np.asarray(self.mario.game_area())
+        np.append =(state_full, self.mario.level_progress)
+        self.state_size = state_full.size
+        
+        
+    def reset(self):
+        self.mario.reset_game()
+        self.done = False
+        self.pyboy.tick()
+        
+        assert self.mario.lives_left == 2
+        
+        state_full = np.asarray(self.mario.game_area())
+        np.append(state_full, self.mario.level_progress)
+        
+        return state_full
+    
+    def step(self, action):
+        if action == 0:
+            self.pyboy.send_input(WindowEvent.PRESS_BUTTON_A) # jump
+            self.time = 50
+        elif action == 1:
+            self.pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT) #move right
+            self.time = 5
+        elif action == 2:
+            self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)#release jump
+            self.time = 5
+        elif action == 3:
+            self.pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)#stop moving right
+            self.time = 5
+        elif action == 4:
+            self.pyboy.send_input(WindowEvent.PRESS_ARROW_LEFT)#move left
+            self.time == 5
+        return action, self.time
+    
+    
+        
+        
+
+
+
+    
