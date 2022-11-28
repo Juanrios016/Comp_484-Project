637e4c1133e02f1c0be80eb659636843d5adc5bf
Joseph Mena Lopez <jmena@macalester.edu>
2022-11-28 16:45:44 -0600

    opens multiple emulators at a time, currently not actually running all simultaneously though

--- MainProgram.py
+++ MainProgram.py
@@ -2,11 +2,59 @@ import numpy as np
 from MarioBrain import MarioBrain
 from GenerateGeneration import GenerateGeneration
 from Enviroment import Environment
+from multiprocessing import Process
 
-def individualEnvBehavior():
+parents = GenerateGeneration()
+trials = [] # for testing
+marioNumber = 0 # current Mario -for testing
+mario1 = MarioBrain()
+mario2 = MarioBrain()
+mario3 = MarioBrain()
+mario4 = MarioBrain()
+mario5 = MarioBrain()
+intitialPopualation = [mario1, mario2, mario3, mario4, mario5]
+initialPopIndex = 0
+
+allscores = [] #for testing to se if fitness score improves for each generation
+
+def individualEnvBehavior(env: Environment, initialPopulation):
     """could be implememnted to run individual emulators at the same time"""
-    pass
+    currMario = initialPopulation
+    state_size = env.state_size
+    state = env.reset()
+    state = np.reshape(state, [1, state_size])
+    actions = currMario.get_actions()
 
+    for act in actions:
+        try:
+            filteredMario = [x for x in list(state[0]) if (x > 10 and x < 30)]
+            index_mario = list(state[0]).index(filteredMario[0])
+            feet_val = state[0][index_mario + 20]
+        except:
+            break   
+
+        env.step(act)
+
+        state = np.asarray(env.mario.game_area())
+        state = np.reshape(state, [1, state_size])
+        
+        # ------ Rendering part -----#
+        i = 0
+        env.render(i, feet_val, env)
+        env.releaseStep(act)
+        env.render(i, feet_val, env)
+        
+        position = env.mario.level_progress # for testing
+        fitness = env.mario.fitness # for testing
+
+    print("fitness: ", fitness) # for testing
+    print("position: ", position) # for testing
+    marioNumber +=1 # for testing
+    trials.append([marioNumber, actions, fitness])# for testing
+    
+    parents.setParents(currMario, fitness)
+    state = env.reset()
+    
 
 
 def main():
@@ -26,44 +74,7 @@ def main():
     env.pyboy.set_emulation_speed(0) 
 
     for l in range(3): # do 3 gen so far
-        for p in range(5): # 5 agents for each gen
-            
-            currMario = intitialPopualation[p]
-            state_size = env.state_size
-            state = env.reset()
-            state = np.reshape(state, [1, state_size])
-            actions = currMario.get_actions()
-
-            for act in actions:
-                try:
-                    filteredMario = [x for x in list(state[0]) if (x > 10 and x < 30)]
-                    index_mario = list(state[0]).index(filteredMario[0])
-                    feet_val = state[0][index_mario + 20]
-                except:
-                    break   
-                           
-                env.step(act)
-
-                state = np.asarray(env.mario.game_area())
-                state = np.reshape(state, [1, state_size])
-                
-                # ------ Rendering part -----#
-                i = 0
-                env.render(i, feet_val, env)
-                env.releaseStep(act)
-                env.render(i, feet_val, env)
-                
-                position = env.mario.level_progress # for testing
-                fitness = env.mario.fitness # for testing
-
-            print("fitness: ", fitness) # for testing
-            print("position: ", position) # for testing
-            marioNumber +=1 # for testing
-            trials.append([marioNumber, actions, fitness])# for testing
-            
-            parents.setParents(currMario, fitness)
-            state = env.reset() 
-
+        ps = [Process(target=individualEnvBehavior, args=(Environment(), intitialPopualation[0])), Process(target=individualEnvBehavior, args=(Environment(), intitialPopualation[1])), Process(target=individualEnvBehavior, args=(Environment(), intitialPopualation[2])), Process(target=individualEnvBehavior, args=(Environment(), intitialPopualation[3])), Process(target=individualEnvBehavior, args=(Environment(), intitialPopualation[4]))]
         parent1, parent2 = parents.getParents()
         intitialPopualation = parents.computeNextGen(parent1, parent2)
 
@@ -78,3 +89,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+
